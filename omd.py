af8d90f571caa741b47670710b719ede30783ce0
Andrey Vasichenko <ad.vasilchenko@yandex.ru>
2020-09-15 19:10:33 +0300

    Update omd.py

--- omd.py
+++ omd.py
@@ -13,13 +13,13 @@ def step1(emoji_list):
         option = input()
     
     if options[option]:
-        return step2_umbrella()
-    return step2_no_umbrella()
+        return step2_umbrella(emoji_list)
+    return step2_no_umbrella(emoji_list)
 
-def step2_no_umbrella():
+def step2_no_umbrella(emoji_list):
     print("Утка не взяла зонтик. Пошел дождь.\n"
           "Утка промокла, надо что-то делать\n"
-          f"Идти домой или купить новый шмот {random.choice(emoji_list)}")y
+          f"Идти домой или купить новый шмот {random.choice(emoji_list)}")
 
     option = ''
     options = {'домой': True, 'новый шмот': False}
@@ -28,11 +28,11 @@ def step2_no_umbrella():
         option = input()
     
     if options[option]:
-        return step3()
-    return step3()
+        return step3(emoji_list)
+    return step3(emoji_list)
 
 
-def step2_umbrella():
+def step2_umbrella(emoji_list):
     print ("Утка взяла зонтик. А дождь как назло не пошел.\n"
            "А зонтик тащить с собой неудобно\n"
            "Выкинуть нафиг или отдать бомжу?")
@@ -44,10 +44,10 @@ def step2_umbrella():
         option = input()
     
     if options[option]:
-        return step3()
-    return step3()
+        return step3(emoji_list)
+    return step3(emoji_list)
 
-def step3():
+def step3(emoji_list):
     print("Вот и сказочке конец. А кто слушал, молодец!")
 
     option = ''
@@ -57,7 +57,7 @@ def step3():
         option = input()
     
     if options[option]:
-        return step1()
+        return step1(emoji_list)
 
 
 if __name__ == '__main__':
