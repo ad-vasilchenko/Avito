90ca920206b7936e2a01cb1e4274175ce2c33d5d
Andrey Vasichenko <ad.vasilchenko@yandex.ru>
2020-09-15 19:06:32 +0300

    Update omd.py

--- omd.py
+++ omd.py
@@ -1,10 +1,11 @@
-# Guido van Rossum <guido@python.org>
+import random
 
-def step1():
-    print(
-        'Утка-маляр 🦆 решила выпить зайти в бар. '
-        'Взять ей зонтик? ☂️'
-    )
+
+def step1(emoji_list):
+    print( "Утка-маляр 🦆 решила выпить зайти в бар.\n"
+           "Взять ей зонтик? \n"
+           f"{random.choice(emoji_list)}" )
+    
     option = ''
     options = {'да': True, 'нет': False}
     while option not in options:
@@ -15,5 +16,50 @@ def step1():
         return step2_umbrella()
     return step2_no_umbrella()
 
+def step2_no_umbrella():
+    print("Утка не взяла зонтик. Пошел дождь.\n"
+          "Утка промокла, надо что-то делать\n"
+          f"Идти домой или купить новый шмот {random.choice(emoji_list)}")y
+
+    option = ''
+    options = {'домой': True, 'новый шмот': False}
+    while option not in options:
+        print('Выберите: {}/{}'.format(*options))
+        option = input()
+    
+    if options[option]:
+        return step3()
+    return step3()
+
+
+def step2_umbrella():
+    print ("Утка взяла зонтик. А дождь как назло не пошел.\n"
+           "А зонтик тащить с собой неудобно\n"
+           "Выкинуть нафиг или отдать бомжу?")
+    
+    option = ''
+    options = {'выкинуть': True, 'отдать': False}
+    while option not in options:
+        print('Выберите: {}/{}'.format(*options))
+        option = input()
+    
+    if options[option]:
+        return step3()
+    return step3()
+
+def step3():
+    print("Вот и сказочке конец. А кто слушал, молодец!")
+
+    option = ''
+    options = {'заново': True, 'закончить': False}
+    while option not in options:
+        print('Выберите: {}/{}'.format(*options))
+        option = input()
+    
+    if options[option]:
+        return step1()
+
+
 if __name__ == '__main__':
-    step1()
+    emoji_list = [  "\U0001f600", "\U0001F606", "\U0001F923", "\U0001F604", "\U0001F605"]
+    step1(emoji_list)
